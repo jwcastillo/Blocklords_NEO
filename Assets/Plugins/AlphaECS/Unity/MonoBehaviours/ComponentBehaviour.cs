0d99121c5db0995cbe5af9ad5fd0ddfba0cc72a6
Tim Riley <tim@finergames.com>
2018-06-24 13:37:45 -0400

    - initial commit - setup gitignore, unity metafiles, and imported neo.lux, alphaecs, unirx, and zenject

---
+++ Assets/Plugins/AlphaECS/Unity/MonoBehaviours/ComponentBehaviour.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace AlphaECS.Unity
+{
+    public abstract class ComponentBehaviour : MonoBehaviour, IDisposable
+    {
+        protected IEventSystem EventSystem { get; set; }
+
+        private CompositeDisposable _disposer = new CompositeDisposable();
+        public CompositeDisposable Disposer
+        {
+            get { return _disposer; }
+            private set { _disposer = value; }
+        }
+			
+        public virtual void OnDestroy()
+        {
+			if (EcsApplication.IsQuitting) { return; }
+
+            Dispose();
+
+			if (EventSystem == null)
+			{
+				if (Debug.isDebugBuild)
+				{
+					Debug.LogWarning ("A COMPONENT ON " + this.gameObject.name + " WAS NOT INJECTED PROPERLY!");
+				}
+				return;
+			}
+            EventSystem.Publish(new ComponentDestroyed() { Component = this });
+        }
+
+        [Inject]
+		public virtual void Initialize(IEventSystem eventSystem)
+        {
+			EventSystem = eventSystem;
+            EventSystem.Publish(new ComponentCreated() { Component = this });
+        }
+
+        public virtual void Dispose()
+        {
+            Disposer.Dispose();
+        }
+
+        public virtual void OnApplicationQuit()
+        {
+			EcsApplication.IsQuitting = true;
+        }
+    }
+}
