933bc36c4d8795eac76113c27d1fa01733089ca5
Medet "Ahmetson" Atabayev <ahmetson@zoho.com>
2018-07-11 12:15:27 +0800

    Revert "Deleting Demo Scripts"
    This reverts commit f903fbf00e98c4617bf6299703abdc79e5451a58.

---
+++ Assets/Demo/Events/BlockchainErrorEvent.cs
@@ -0,0 +1,9 @@
+﻿
+public class BlockchainErrorEvent {
+    public string Message { get; private set; }
+
+    public BlockchainErrorEvent(string message)
+    {
+        this.Message = message;
+    }
+}
