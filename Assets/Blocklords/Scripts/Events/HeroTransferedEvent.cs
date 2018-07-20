18341bd4420ca824448600937516fe56fa52e909
Medet "Ahmetson" Atabayev <ahmetson@zoho.com>
2018-07-21 06:02:17 +0800

    Added Blockchain Middleware that works with Heroes

---
+++ Assets/Blocklords/Scripts/Events/HeroTransferedEvent.cs
@@ -0,0 +1,22 @@
+﻿
+public class HeroTransferedEvent {
+    public bool isSuccess;
+    public string message;
+
+    public HeroTransferedEvent(bool iS, string m )
+    {
+        this.isSuccess = iS;
+        this.message = m;
+    }
+
+    public HeroTransferedEvent(string m)
+    {
+        this.isSuccess = false;
+        this.message = m;
+    }
+    public HeroTransferedEvent()
+    {
+        this.isSuccess = true;
+        this.message = "";
+    }
+}
